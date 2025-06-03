d3bc561c18a3e1ae5bd9378e2077b55ca1fb361f
Mohtasin Bari <24-58305-2@student.aiub.edu>
2025-06-03 22:19:57 +0600

    Create Start.java

---
+++ Start.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+import Entity.*;
+import GUI.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class Start {
+    public static void main(String[] args) {
+        new SkinCareShop();
+    }
+}
